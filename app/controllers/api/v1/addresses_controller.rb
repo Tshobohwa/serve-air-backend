82941ffcdb05ab6f90617b850cc9478c58a04980
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-05-04 18:02:24 +0200

    Add Address controller

---
+++ app/controllers/api/v1/addresses_controller.rb
@@ -0,0 +1,50 @@
+class Api::V1::AddressesController < ApplicationController
+  before_action :set_address, only: [:show, :update, :destroy]
+
+  # GET api/v1/addresses
+  def index
+    @addresses = Address.all
+    render json: { status: 'success', data: { addresses: @addresses}}
+  end
+
+  # GET api/v1/addresses/:id
+  def show
+    render json: {status: 'success', data: {address: @address} }
+  end
+
+  # POST api/v1/addresses
+  def post
+    @address = Address.new(address_params)
+    if @address.save
+      render json: { status: 'success', data: { status: @status}}, status: :created
+    else
+      render json: {status: 'fail', error: @address.errors}, status: :unprocessable_entity
+    end
+  end
+
+  # PUT/PATCH api/v1/:id
+  def update
+    if @address.update(address_params)
+      render json: {status: 'success', data: {address: @address}}
+    else
+      render json: {status: 'fail', errors: @address.errors}, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE api/v1/:id
+  def destroy
+    @address.destroy
+  end
+
+  private
+
+  def set_address
+    @address = Address.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      render json: { status:"fail", message: "Address with id #{params[:id]} not found"}, status: :not_found
+  end
+
+  def address_params
+    params.require(:address).permit(:name)
+  end
+end
