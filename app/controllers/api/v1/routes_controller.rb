b328b9f8d8a5479bbb15d2dd4442c33eb2882d9d
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-05-04 21:36:02 +0200

    Create routes controller

---
+++ app/controllers/api/v1/routes_controller.rb
@@ -0,0 +1,55 @@
+class Api::V1::RoutesController < ApplicationController
+  before_action :set_route, only: [:show, :destroy, :update ]
+  # GET api/v1/routes
+  def index
+    @routes = Route.all
+    render json: {status: 'success', data: { routes: @routes }}
+  end
+
+  # GET api/v1/routes/:id
+  def show
+    render json: {status: 'success', data: { route: @route }}
+  end
+
+  # DELETE api/v1/routes/:id
+  def destroy
+    @route.destroy
+  end
+
+  # POST api/v1/:id
+  def create
+    @route = Route.new(route_params)
+    rescue ActiveRecord::RecordNotUnique
+      render json: {status: 'fail', error: {message: 'Route already exists'}}, status: :unprocessable_entity
+
+    if @route.save
+      render json: {status: 'success', data: { route: @route}}
+    else
+      render json: {status: 'fail', error: @route.errors}, status: :unprocessable_entity
+    end
+  end
+
+
+  # PUT/PATCH api/v1/:id
+  def update
+    if @route.update(route_params)
+      render json: {status: 'success', data: { route: @route}}
+    else
+      render json: {status: 'fail', error: @route.errors}, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  # Set route from request param i:d
+  def set_route
+    @route = Route.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      render json: {status: 'fail', error: { message: "Couldn't find route with id #{params[:id]}"}}
+  end
+
+  #Sanitaze request parameters
+  def route_params
+    params.require(:route).permit(:origin_id, :destination_id, :pricing)
+  end
+end
--- app/models/route.rb
+++ app/models/route.rb
@@ -1,5 +1,5 @@
 class Route < ApplicationRecord
-  validate :pricing, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :pricing, presence: true, numericality: { greater_than_or_equal_to: 0 }
   has_many :addresses, foreign_key: :origin_id
   has_many :addresses, foreign_key: :destination_id
 end
