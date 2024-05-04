858829b2f66b1c7e18de74c440a2d5e34aa0528b
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-05-04 13:33:03 +0200

    finish address model

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,7 @@
 class Address < ApplicationRecord
   validates :name, presence: true
+  has_many :users, foreign_key: :address_id
+  has_many :packages, foreign_key: :current_address_id
+  has_many :routes, foreign_key: :origin_id
+  has_many :routes, foreign_key: :destination_id
 end
