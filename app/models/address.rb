9cfa143093954a652ce591c2a33cca17fa886868
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-05-06 09:22:41 +0200

    Destroy dependent routes on address delete

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,6 +2,6 @@ class Address < ApplicationRecord
   validates :name, presence: true
   has_many :users, foreign_key: :address_id
   has_many :packages, foreign_key: :current_address_id
-  has_many :routes, foreign_key: :origin_id
+  has_many :routes, foreign_key: :origin_id, dependent: :destroy
   has_many :routes, foreign_key: :destination_id
 end
