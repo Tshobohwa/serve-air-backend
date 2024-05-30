d2bcd12f2c7f110790d6a35f0d0233686b81a9d1
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-05-30 20:33:32 +0200

    Fix association error

--- app/controllers/api/v1/destinations_controller.rb
+++ app/controllers/api/v1/destinations_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::DestinationsController < ApplicationController
   # GET api/v1/destinations
   def index
-    @destinations = Destination.includes(:addresses)
+    @destinations = Destination.includes(:address)
 
     render json: { status: "success", data: { destinations: @destinations.as_json(include: :address) } }
   end
